182805a1583f4057cc228ec1ef33d0aa8f3a2315
Bhuvan1326 <bhuvanpatil1313@gmail.com>
2026-02-12 23:11:55 +0530

    Updated JS file

--- OOP in JS.js
+++ OOP in JS.js
@@ -1,12 +1,7 @@
-let b=document.createElement("button");
-b.innerHTML="submit";
-document.body.append(b);
-
-let p=document.createElement("p")
-p.innerHTML="This is a p tag before button";
-b.before(p);
-
-
-let h1=document.createElement("h1");
-h1.innerHTML="Good Morning";
-b.after(h1);
+let student={
+    name:"Bhuvan Patil",
+    marks:91,
+    printMarks: function(){
+        console.log("marks = ",this.marks);
+    },
+};
